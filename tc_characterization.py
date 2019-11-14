6318b44af27fc11f21869081f0f091f5a203e808
Eric March <eric.march91@gmail.com>
2019-11-14 14:28:54 +0100

    new functionality

--- tc_characterization.py
+++ tc_characterization.py
@@ -2,24 +2,32 @@
 import pandas as pd
 import pubchempy as pcp
 
-from chembl_webresource_client.new_client import new_client
+# from chembl_webresource_client.new_client import new_client
 
-molecule = new_client.molecule
-molecule.set_format('json')
-res = molecule.search('aspirin')
-
-
-df = pd.io.json.json_normalize(res)
+# molecule = new_client.molecule
+# molecule.set_format('json')
+# res = molecule.search('aspirin')
 
 
-print(df[["pref_name",
-            "molecule_chembl_id", 
-            "molecule_structures.canonical_smiles", 
-            "molecule_structures.standard_inchi_key",
-            "molecule_properties.acd_most_apka",
-            "molecule_properties.alogp",
-            "molecule_properties.mw_freebase"]])
-print(df.columns)
+# df = pd.io.json.json_normalize(res)
 
-results = pcp.get_compounds('aspirin', 'name')
-print(results)
+
+# print(df[["pref_name",
+#             "molecule_chembl_id", 
+#             "molecule_structures.canonical_smiles", 
+#             "molecule_structures.standard_inchi_key",
+#             "molecule_properties.acd_most_apka",
+#             "molecule_properties.alogp",
+#             "molecule_properties.mw_freebase"]])
+# print(df.columns)
+
+with open('CAS_no_struc.csv', 'r') as cas_file:
+
+    for cas in cas_file:
+        cas_num = cas.strip()
+        results = pcp.get_compounds(cas_num, 'name')
+        if results:
+            for compound in results:
+                print(compound.isomeric_smiles)
+        else:
+            print(cas_num)
